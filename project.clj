337faf85b3896f23ba66bd8e7ff79a6b1fd288ca
Inge Solvoll <inge.solvoll@gmail.com>
2018-02-27 23:31:59 +0100

    Remove all the extras that I maybe don't need

--- project.clj
+++ project.clj
@@ -11,25 +11,4 @@
                  [venantius/accountant "0.1.9"]
                  [org.clojure/core.match "0.3.0-alpha5"]]
 
-  :source-paths ["src"]
-
-  :clean-targets ^{:protect false} [:target-path :compile-path "resources/public/js/compiled"]
-
-  :deploy-repositories [["releases" {:sign-releases false :url "https://clojars.org/repo"}
-                         ["snapshots" {:sign-releases false :url "https://clojars.org/repo"}]]]
-
-  :release-tasks [["vcs" "assert-committed"]
-                  ["change" "version" "leiningen.release/bump-version" "release"]
-                  ["vcs" "commit"]
-                  ["vcs" "tag" "v" "--no-sign"]
-                  ["deploy"]
-                  ["change" "version" "leiningen.release/bump-version"]
-                  ["vcs" "commit"]
-                  ["vcs" "push"]]
-
-
-  :cljsbuild {:builds [{:id           "min"
-                        :source-paths ["src"]
-                        :compiler     {:output-to      "resources/public/js/compiled/app.js"
-                                       :optimizations  :advanced
-                                       :parallel-build true}}]})
+  :source-paths ["src"])
