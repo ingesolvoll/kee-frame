2d7a3f9f57d8aca20cb8fbb16b1ef91cca396889
Inge Solvoll <inge.solvoll@gmail.com>
2018-03-04 21:49:15 +0100

    Nicer access to paths

--- src/kee_frame/core.cljs
+++ src/kee_frame/core.cljs
@@ -1,8 +1,8 @@
 (ns kee-frame.core
   (:require [kee-frame.state :as state]
             [re-frame.core :as rf]
-            [kee-frame.controller :as controller]))
-
+            [kee-frame.controller :as controller]
+            [bidi.bidi :as bidi]))
 
 (defn start! [routes]
   (controller/start-router! routes))
@@ -11,3 +11,6 @@
 
 (defn reg-controller [id controller]
   (swap! state/controllers assoc id controller))
+
+(defn path-for [handler & params]
+  (apply bidi/path-for @state/routes handler params))
