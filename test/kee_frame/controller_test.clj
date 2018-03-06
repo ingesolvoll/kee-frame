6eb798bb8b5222b6fbb25822f77a45a1084a0d6e
Inge Solvoll <inge.solvoll@gmail.com>
2018-03-06 01:23:22 +0100

    Found a way to test rf side effects

--- test/kee_frame/controller_test.clj
+++ test/kee_frame/controller_test.clj
@@ -1,12 +1,17 @@
 (ns kee-frame.controller-test
   (:require [clojure.test :refer :all]
-            [kee-frame.controller :as c]))
+            [kee-frame.controller :as c]
+            [re-frame.core :as rf]))
 
-(def controllers {:my-controller {:params (constantly true)
+(def controllers {:my-controller {:params #(-> % :handler (= :some-page))
                                   :start  [:start/event]
                                   :stop   [:stop/event]}})
 
 (deftest hey
-  (is (true? (-> (c/apply-route controllers {} {:handler :some-page})
-                 :my-controller
-                 :last-params))))
+  (let [events (atom [])]
+    (with-redefs [rf/dispatch #(swap! events conj %)]
+      (let [controllers-after-first-route (c/apply-route controllers {} {:handler :some-page})]
+        (is (= [[:start/event true]] @events))
+        (c/apply-route controllers-after-first-route {} {:handler :other-page})
+        (is (= [[:start/event true]
+                [:stop/event]] @events))))))
