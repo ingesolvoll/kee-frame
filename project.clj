dd91d438644717f4e638685566c6954044ad7d68
Inge Solvoll <inge.solvoll@gmail.com>
2018-03-06 01:11:07 +0100

    Not touching reagent

--- project.clj
+++ project.clj
@@ -5,8 +5,7 @@
   :url "https://github.com/ingesolvoll/kee-frame"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojurescript "1.9.946"]
-                 [reagent "0.8.0-alpha2"]
-                 [re-frame "0.10.3-alpha1" :exclusions [reagent]]
+                 [re-frame "0.10.3-alpha1"]
                  [bidi "2.0.16"]
                  [venantius/accountant "0.1.9"]
                  [org.clojure/core.match "0.3.0-alpha5"]]
