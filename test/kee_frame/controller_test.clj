8a74a6b259c5c57ce5b68e203795dc2a8f2d3f55
Inge Solvoll <inge.solvoll@gmail.com>
2018-03-06 10:05:18 +0100

    More controller tests

--- test/kee_frame/controller_test.clj
+++ test/kee_frame/controller_test.clj
@@ -3,15 +3,24 @@
             [kee-frame.controller :as c]
             [re-frame.core :as rf]))
 
-(def controllers {:my-controller {:params #(-> % :handler (= :some-page) (or nil))
-                                  :start  [:start/event]
-                                  :stop   [:stop/event]}})
+(deftest compact-syntax
+  (testing "Can start and stop"
+    (let [events (atom [])]
+      (with-redefs [rf/dispatch #(swap! events conj %)]
+        (-> {:my-controller {:params #(-> % :handler (= :some-page) (or nil))
+                             :start  [:start/event]
+                             :stop   [:stop/event]}}
+            (c/apply-route {} {:handler :some-page})
+            (c/apply-route {} {:handler :other-page}))
+        (is (= [[:start/event true]
+                [:stop/event]] @events)))))
 
-(deftest hey
-  (let [events (atom [])]
-    (with-redefs [rf/dispatch #(swap! events conj %)]
-      (-> controllers
-          (c/apply-route {} {:handler :some-page})
-          (c/apply-route {} {:handler :other-page}))
-      (is (= [[:start/event true]
-              [:stop/event]] @events)))))
+  (testing "Will trigger only once when always on"
+    (let [events (atom [])]
+      (with-redefs [rf/dispatch #(swap! events conj %)]
+        (-> {:always-on-controller {:params (constantly true)
+                                    :start  [:start/event]}}
+            (c/apply-route {} {:handler :some-page})
+            (c/apply-route {} {:handler :other-page})
+            (c/apply-route {} {:handler :third-page}))
+        (is (= [[:start/event true]] @events))))))
