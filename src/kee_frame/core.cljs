67c2ebb23b16d64b9607d19297460003f3095985
Inge Solvoll <inge.solvoll@gmail.com>
2018-03-08 19:16:38 +0100

    We won't be doing that, not the focus of this lib

--- src/kee_frame/core.cljs
+++ src/kee_frame/core.cljs
@@ -1,6 +1,5 @@
 (ns kee-frame.core
   (:require [kee-frame.state :as state]
-            [re-frame.core :as rf]
             [kee-frame.router :as router]))
 
 (defn start! [routes initial-db]
@@ -8,9 +7,6 @@
 
 (defn reg-controller [id controller]
   (swap! state/controllers assoc id controller))
-
-(defn reg-swap [id f & args]
-  (rf/reg-event-db id [rf/debug] (fn [db] (apply f db args))))
 
 (defn path-for [handler & params]
   (apply router/url handler params))
