94e51fb26a5de192a85cd9f4c969b2cec87f8058
Inge Solvoll <inge.solvoll@gmail.com>
2018-03-10 23:59:57 +0100

    Chain api into core

--- src/kee_frame/core.cljs
+++ src/kee_frame/core.cljs
@@ -1,12 +1,16 @@
 (ns kee-frame.core
   (:require [kee-frame.state :as state]
-            [kee-frame.router :as router]))
+            [kee-frame.router :as router]
+            [kee-frame.chain :as chain]))
 
 (defn start! [options]
   (router/start! options))
 
 (defn reg-controller [id controller]
   (swap! state/controllers assoc id controller))
+
+(defn reg-chain [id & handlers]
+  (apply chain/reg-chain-2 id handlers))
 
 (defn path-for [handler & params]
   (apply router/url handler params))
