d08406a14f23c4925a824df069ccb90880665115
Inge Solvoll <inge.solvoll@gmail.com>
2018-04-09 23:38:24 +0200

    Smoother testing

--- src/kee_frame/state.cljc
+++ src/kee_frame/state.cljc
@@ -13,3 +13,8 @@
 (def links (atom [{:effect-present?   (fn [effects] (:http-xhrio effects))
                    :explicit-dispatch (fn [effects] (get-in effects [:http-xhrio :on-success]))
                    :insert-dispatch   (fn [effects dispatch] (assoc-in effects [:http-xhrio :on-success] dispatch))}]))
+
+(defn reset-state! []
+  (reset! controllers {})
+  (reset! router nil)
+  (reset! navigator nil))
--- test/kee_frame/navigation_test.clj
+++ test/kee_frame/navigation_test.clj
@@ -16,24 +16,24 @@
 
 (deftest can-navigate-through-events
   (testing "Good navigation"
-    (with-redefs [state/controllers
-                  (atom {:test-controller {:params #(when (= (:handler %) :some-route)
-                                                      (:route-params %))
-                                           :start  [:test-event-2]}})]
-      (rf-test/run-test-sync
-        (k/start! {:routes routes})
-        (rf/reg-sub :test-prop :test-prop)
-        (k/reg-event-fx :test-event
-                        (fn [_ _] {:navigate-to [:some-route :id 1]}))
-        (k/reg-event-fx :test-event-2
-                        (fn [_ [args]] {:db {:test-prop args}}))
-        (rf/dispatch [:test-event])
-        (is (= {:id "1"} @(rf/subscribe [:test-prop]))))))
+    (state/reset-state!)
+    (k/reg-controller :test-controller {:params #(when (= (:handler %) :some-route)
+                                                   (:route-params %))
+                                        :start  [:test-event-2]})
+    (rf-test/run-test-sync
+      (k/start! {:routes routes})
+      (rf/reg-sub :test-prop :test-prop)
+      (k/reg-event-fx :test-event
+                      (fn [_ _] {:navigate-to [:some-route :id 1]}))
+      (k/reg-event-fx :test-event-2
+                      (fn [_ [args]] {:db {:test-prop args}}))
+      (rf/dispatch [:test-event])
+      (is (= {:id "1"} @(rf/subscribe [:test-prop])))))
 
   (testing "Bad dispatch value returned form start fn throws exception"
-    (with-redefs [state/controllers
-                  (atom {:test-controller {:params (constantly true)
-                                           :start  (fn [_ _] "This is not cool")}})]
-      (rf-test/run-test-sync
-        (is (thrown-with-msg? ExceptionInfo #"Invalid dispatch value"
-                              (k/start! {:routes routes})))))))
+    (state/reset-state!)
+    (k/reg-controller :test-controller {:params (constantly true)
+                                        :start  (fn [_ _] "This is not cool")})
+    (rf-test/run-test-sync
+      (is (thrown-with-msg? ExceptionInfo #"Invalid dispatch value"
+                            (k/start! {:routes routes}))))))
