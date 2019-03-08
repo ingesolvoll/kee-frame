9dbc8973f21de322437ca7b5eb8ee85395ad8e9b
ingesolvoll <inge@semion.ca>
2019-03-08 10:32:28 +0100

    Dead code

--- test/kee_frame/alpha_test.clj
+++
@@ -1,7 +0,0 @@
-(ns kee-frame.alpha-test
-  (:require [clojure.test :refer :all]
-            [kee-frame.alpha :as m]))
-
-(deftest haha
-  (let [f (m/fn-assoc :mordi [:kee-frame.core/params 1])]
-    (is (= {:mordi 2} (f {} [1 2])))))
