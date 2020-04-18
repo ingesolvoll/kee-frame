f73ed39dd8b30e5e0ae5680ed1fe224615a8452a
ingesolvoll <inge@semion.ca>
2020-04-18 18:24:10 +0200

    #86 Bumped all deps

--- project.clj
+++ project.clj
@@ -4,21 +4,21 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/ingesolvoll/kee-frame"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.597"]
-                 [re-frame "0.10.6"]
-                 [re-chain "1.1-SNAPSHOT"]
-                 [metosin/reitit-core "0.2.13"]
-                 [day8.re-frame/http-fx "0.1.6"]
+                 [re-frame "0.12.0"]
+                 [re-chain "1.1"]
+                 [metosin/reitit-core "0.4.2"]
+                 [day8.re-frame/http-fx "v0.2.0"]
                  [cljs-ajax "0.8.0"]
-                 [venantius/accountant "0.2.4"]
-                 [org.clojure/core.match "0.3.0-alpha5"]
-                 [expound "0.8.2"]
+                 [venantius/accountant "0.2.5"]
+                 [org.clojure/core.match "1.0.0"]
+                 [expound "0.8.4"]
                  [day8.re-frame/test "0.1.5"]
                  [breaking-point "0.1.2"]
                  [pez/clerk "1.0.0"]]
 
-  :plugins [[com.jakemccrary/lein-test-refresh "0.20.0"]]
+  :plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]]
   :deploy-repositories [["clojars" {:sign-releases false
                                     :url           "https://clojars.org/repo"
                                     :username      :env/clojars_username
