f017e5da90c7df5e88dc0a719bdd124a9282dd13
ingesolvoll <inge@semion.ca>
2020-10-14 23:23:46 +0200

    The breaking change in re-chain deserves a minor bump.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kee-frame "1.0.1-SNAPSHOT"
+(defproject kee-frame "1.1.0"
   :description "A micro-framework on top of re-frame"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
