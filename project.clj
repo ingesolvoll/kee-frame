6755bb9ee40e4a831189f6eb6485c6575e17cd6e
ingesolvoll <inge@semion.ca>
2020-10-26 23:06:20 +0100

    Next snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kee-frame "1.1.1"
+(defproject kee-frame "1.1.2-SNAPSHOT"
   :description "A micro-framework on top of re-frame"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
