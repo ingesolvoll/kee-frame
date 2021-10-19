e7c7d23b42875bb2eb1607c8615e8c9b817d591a
ingesolvoll <inge.solvoll@gmail.com>
2021-10-19 22:08:41 +0200

    Another minor 1.3.1 to bump deps

--- build.clj
+++ build.clj
@@ -4,7 +4,7 @@
 
 (def lib 'kee-frame/kee-frame)
 ;; if you want a version of MAJOR.MINOR.COMMITS:
-(def version (format "1.3.0"))
+(def version (format "1.3.1"))
 
 (defn install [opts]
   (-> opts
