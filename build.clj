59291ba06dbb0124628bd5e9c20a4e9d2ec6bd32
ingesolvoll <inge.solvoll@gmail.com>
2021-10-19 21:49:14 +0200

    1.3.0 version

--- build.clj
+++ build.clj
@@ -4,7 +4,7 @@
 
 (def lib 'kee-frame/kee-frame)
 ;; if you want a version of MAJOR.MINOR.COMMITS:
-(def version (format "1.2.1"))
+(def version (format "1.3.0"))
 
 (defn install [opts]
   (-> opts
