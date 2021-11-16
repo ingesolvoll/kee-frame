27eec9a3d2144a33a94c4d44546e9b04cd9a87a3
ingesolvoll <inge.solvoll@gmail.com>
2021-11-16 10:31:18 +0100

    Version of with-fsm that assigns a subscription

--- src/kee_frame/fsm/beta.clj
+++ src/kee_frame/fsm/beta.clj
@@ -1,11 +1,20 @@
 (ns kee-frame.fsm.beta
-  (:require [re-frame.core :as f]))
+  (:require [clojure.spec.alpha :as s]
+            [re-frame.core :as f]))
 
-(defmacro with-fsm [fsm & body]
-  (let [id (:id fsm)]
-    `(reagent.core/with-let [_# (f/dispatch [::start ~fsm])]
-       ~@body
-       ~(list
-         'finally
-         `(re-frame.core/dispatch [::stop ~id])))))
+(s/def ::binding (s/and vector?
+                        (s/cat :subscription-symbol symbol? :fsm any?)))
+
+(defmacro with-fsm [binding & body]
+  (let [parsed (s/conform ::binding binding)]
+    (when (= ::s/invalid parsed)
+      (throw (ex-info "with-fsm accepts exactly one binding pair, the subscription symbol and the FSM declaration."
+                      (s/explain-data ::binding binding))))
+    (let [{:keys [:subscription-symbol :fsm]} parsed]
+      `(reagent.core/with-let [~subscription-symbol (f/subscribe [::state (:id ~fsm)])
+                               _# (f/dispatch [::start ~fsm])]
+         ~@body
+         ~(list
+           'finally
+           `(re-frame.core/dispatch [::stop (:id ~fsm)]))))))
 
