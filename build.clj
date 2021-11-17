299439de65bf84e46ae0ed50c19a0b67e37e48a7
ingesolvoll <inge.solvoll@gmail.com>
2021-11-17 12:49:54 +0100

    CircleCI deploy

--- build.clj
+++ build.clj
@@ -1,6 +1,8 @@
 (ns build
-  (:require [clojure.tools.build.api :as b]
-            [org.corfield.build :as bb]))
+  (:require
+   [clojure.string :as str]
+   [clojure.tools.build.api :as b]
+   [org.corfield.build :as bb]))
 
 (def lib 'kee-frame/kee-frame)
 ;; if you want a version of MAJOR.MINOR.COMMITS:
@@ -12,9 +14,25 @@
       (bb/jar)
       (bb/install)))
 
-(defn ci [opts]
-  (-> opts
-      (assoc :lib lib :version version)
-      (bb/run-tests)
-      (bb/jar)
-      (bb/deploy)))
+(def release-marker "Release-")
+
+(defn extract-version [tag]
+  (str/replace-first tag release-marker ""))
+
+(defn maybe-deploy [opts]
+  (if-let [tag (System/getenv "CIRCLE_TAG")]
+    (do
+      (println "Found tag " tag)
+      (if (re-find (re-pattern release-marker) tag)
+        (do
+          (println "Releasing to clojars...")
+          (-> opts
+              (assoc :lib lib :version (extract-version tag))
+              (bb/jar)
+              (bb/deploy)))
+        (do
+          (println "Tag is not a release tag, skipping deploy")
+          opts)))
+    (do
+      (println "No tag found, skipping deploy")
+      opts)))
